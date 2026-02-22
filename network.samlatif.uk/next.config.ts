ecc98b3608a62eeecdad8b3052cd97b2fa3e577d
Sam Latif <hello@samlatif.uk>
2026-02-22 09:44:09 +0000

    feat: implement Content Security Policy and Strict Transport Security headers for enhanced security

--- network.samlatif.uk/next.config.ts
+++ network.samlatif.uk/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests; block-all-mixed-content",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
