5a2ddcb56b67aaabfa9427f39fb71f8ca0b28267
Sam Latif <hello@samlatif.uk>
2026-02-25 12:54:55 +0000

    feat: update Overview component to dynamically display public recommendations count

--- react.samlatif.uk/src/components/Overview.tsx
+++ react.samlatif.uk/src/components/Overview.tsx
@@ -1,24 +1,32 @@
-export const Overview = () => (
-  <section id="overview">
-    <div className="container">
-      <div className="stats">
-        <div className="stat">
-          <div className="sn">15+</div>
-          <div className="sl">Years Experience</div>
-        </div>
-        <div className="stat">
-          <div className="sn">25+</div>
-          <div className="sl">Client Engagements</div>
-        </div>
-        <div className="stat">
-          <div className="sn">5</div>
-          <div className="sl">Finance Institutions</div>
-        </div>
-        <div className="stat">
-          <div className="sn">MSc</div>
-          <div className="sl">1st Class BSc</div>
+import { TESTIMONIALS } from "../data/cv";
+
+export const Overview = () => {
+  const recommendationsCount = TESTIMONIALS.filter(
+    (testimonial) => testimonial.visibility === "public",
+  ).length;
+
+  return (
+    <section id="overview">
+      <div className="container">
+        <div className="stats">
+          <div className="stat">
+            <div className="sn">15+</div>
+            <div className="sl">Years Experience</div>
+          </div>
+          <div className="stat">
+            <div className="sn">25+</div>
+            <div className="sl">Client Engagements</div>
+          </div>
+          <div className="stat">
+            <div className="sn">5</div>
+            <div className="sl">Finance Institutions</div>
+          </div>
+          <div className="stat">
+            <div className="sn">{recommendationsCount}</div>
+            <div className="sl">Recommendations</div>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
